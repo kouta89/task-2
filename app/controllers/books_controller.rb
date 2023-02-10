dc1fff2928839bf4970dd47c3405335a5b0996ec
geromon <123849679+geromon89@users.noreply.github.com>
2023-02-10 06:25:25 +0000

    task-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,37 @@
 class BooksController < ApplicationController
   def new
+
+  end
+
+  def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
-  end
-
-  def index
+    redirect_to show_book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to show_book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
