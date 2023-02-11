09538344459634b52a69eb3a9e23243f50af5a58
geromon <123849679+geromon89@users.noreply.github.com>
2023-02-11 07:33:02 +0000

    [finish]task-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,10 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:hoge] = "Book was successfully updated."
-    redirect_to show_book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:hoge] = "Book was successfully updated."
+      redirect_to show_book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
